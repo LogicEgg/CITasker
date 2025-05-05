c7ccbc0d8d3366f44ce6b16c98de5b8236d2e806
Bill <bliao6@my.bcit.ca>
2025-05-05 14:57:51 -0700

    trackers in course list pages now working

--- models/course.py
+++ models/course.py
@@ -1,6 +1,9 @@
 from db import db
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, select
 from sqlalchemy.orm import mapped_column, relationship
+from .event import Event
+import operator
+from datetime import datetime
 
 class Course(db.Model):
     __tablename__ = "courses"
@@ -10,3 +13,15 @@ class Course(db.Model):
     term = mapped_column(Integer, nullable=False)
     # events = relationship("Event")
     # class_list = relationship("Classlist", back_populates="course")
+
+    def task_count(self):
+        counter = [i for i in db.session.execute(select(Event).where(Event.courseid == self.id)).scalars() if not i.completed]
+        return len(counter)
+    
+    def upcoming(self):
+        next_task = [i for i in db.session.execute(select(Event).where(Event.courseid == self.id)).scalars() if not i.completed]
+        next_task.sort(key=operator.attrgetter('deadline'))
+        if next_task:
+            return datetime.strftime(next_task[0].deadline, "%A %d, %b %Y, %I:%M%p %Z")
+        else:
+            return "Nothing upcoming."
