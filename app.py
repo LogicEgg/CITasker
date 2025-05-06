f960793149293f166d6234e8485e96e8e0de0381
Bill <bliao6@my.bcit.ca>
2025-05-06 12:12:51 -0700

    nuke 0.0.0.0

--- app.py
+++ app.py
@@ -14,23 +14,19 @@ app.register_blueprint(api_bp)
 
 db.init_app(app)
 
-
 @app.route("/")
 def homepage():
     urgent = db.session.execute(select(Event).where(Event.deadline < datetime.now() + timedelta(days=4))).scalars()
     return render_template("index.html", upcoming=urgent)
 
-
 @app.route("/terms")
 def term_page():
     return render_template("terms.html")
-
 
 @app.route("/terms/<int:num>")
 def term_classes(num):
     data = db.session.execute(select(Course).where(Course.term == num)).scalars()
     return render_template("classes.html", data=data, num=num)
-
 
 @app.route("/classes/<int:id>", methods=["GET"])
 def class_page(id):
@@ -38,6 +34,5 @@ def class_page(id):
     events = db.session.execute(select(Event).where(Event.courseid == id)).scalars()
     return render_template("class_page.html", course=course, events=events)
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=3000, host="0.0.0.0")
+    app.run(debug=True, port=3000)
